944c8dc9c16f638559ebe7d2a5b2e4576461b526
MarcoRomanoHub <marcoromano270@gmail.com>
2024-03-25 22:22:03 +0100

    [add] console log cardsList

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <script>
+import axios from 'axios';
+import { store } from './data/store';
 import Header from './components/Header.vue'
 import Main from './components/Main.vue';
 export default {
   components: {
     Header,
-    Main
+    Main,
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    getApi() {
+
+      axios.get(this.store.apiUrl, {
+        params: {
+
+        }
+      })
+        .then(result => {
+          this.store.cardsList = result.data.results
+          console.log(this.store.cardsList);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
+  },
+  mounted() {
+    this.getApi()
   }
 }
 
