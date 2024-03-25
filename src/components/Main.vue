bb8be5dcf60080ec467de890465129aa48f787ae
MarcoRomanohub <153228493+MarcoRomanohub@users.noreply.github.com>
2024-03-25 19:58:15 +0100

    Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<script>
+import { store } from '../data/store'
+export default {
+  data() {
+    return {
+      store
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div class="text-center my-5 ">
+    <button @click="store.counter++" class="btn btn-primary"> incrementa contatore </button>
+  </div>
+
+</template>
+
+<style lang="scss" scoped></style>
